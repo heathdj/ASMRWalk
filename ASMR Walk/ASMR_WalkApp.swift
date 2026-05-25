8d6a77ac14e885127db4cc84e9384b9c253d6d8a
heathdj <heathdj@gmail.com>
2026-05-24 20:32:41 -0500

    Initial Commit

---
+++ ASMR Walk/ASMR_WalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASMR_WalkApp.swift
+//  ASMR Walk
+//
+//  Created by David Heath on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct ASMR_WalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASMR WalkTests/ASMR_WalkTests.swift
@@ -0,0 +1,19 @@
+//
+//  ASMR_WalkTests.swift
+//  ASMR WalkTests
+//
+//  Created by David Heath on 5/24/26.
+//
+
+import Testing
+@testable import ASMR_Walk
+
+struct ASMR_WalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
