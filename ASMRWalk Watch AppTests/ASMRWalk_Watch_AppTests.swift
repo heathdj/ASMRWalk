f369be370c14f612ad771714b67fa0bbe48045ab
heathdj <heathdj@gmail.com>
2026-08-29 18:06:19 -0500

    Add Watch app target shell

---
+++ ASMRWalk Watch AppTests/ASMRWalk_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  ASMRWalk_Watch_AppTests.swift
+//  ASMRWalk Watch AppTests
+//
+//  Created by David Heath on 8/29/26.
+//
+
+import Testing
+@testable import ASMRWalk_Watch_App
+
+struct ASMRWalk_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
